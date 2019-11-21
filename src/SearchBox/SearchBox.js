a40e3d5e62daf1a1933645f90fe30db0242305d9
Ross Mawdsley <rossmawd@gmail.com>
2019-11-21 22:35:18 +0000

    Semantic React Header and Input added

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -1,12 +1,17 @@
 // @flow
 import React from 'react';
 import { Button } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 
 const ButtonExampleButton = () => <Button>Find Their Favourite</Button>
 
 const SearchBox = () => {
   return (
     <div>
+      <Header as='h1'>Enter a GitHub UserName to Find their Favouite Language!</Header>
+      <Input placeholder='Search...' />
+      <br></br>
       {ButtonExampleButton()}
     </div>
   );
